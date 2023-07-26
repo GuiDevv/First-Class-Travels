e8613ebf39a9141d6b992d77a522289404cb2e40
Guilherme Gonçalves <guilherme_1300@hotmail.com>
2023-07-26 04:43:24 -0300

    Header 100% - Responsive : )
    Finally, I was able to finish this...

---
+++ js/header.js
@@ -0,0 +1,6 @@
+function onMenuClick() {
+    var navbar = document.getElementById("navigation_tab");
+    var responsive_class_name = "responsive";
+
+    navbar.classList.toggle(responsive_class_name);
+}
