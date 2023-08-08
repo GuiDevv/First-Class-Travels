e5121f27e93e851a6c6173a711d5c23e9831e2d0
Guilherme Gonçalves <guilherme_1300@hotmail.com>
2023-08-08 04:47:32 -0300

    Question Responsive

--- js/questions.js
+++ js/questions.js
@@ -2,7 +2,7 @@ function onQuestionClick(self) {
     console.log(self.children[0].children[1].style);
     if(self.children[1].style.display == "none" || self.children[1].style.display == ""){
         self.children[0].children[1].style.transform = "rotate(90deg)";
-        self.children[1].style.display = "block";
+        self.children[1].style.display = "flex";
     }        
     else{
         self.children[0].children[1].style.transform = "rotate(0deg)";
